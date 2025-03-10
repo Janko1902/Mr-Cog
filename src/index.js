da8f1c99552f44208b60445ae19b4026f9b94647
Janko <161398674+Janko1902@users.noreply.github.com>
2025-03-10 13:02:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ client.on("interactionCreate", (interaction) => {
     }
   }
 
-  if (interaction.commandName === "server") {
+  if (interaction.commandName === "servers") {
 
     const embed = new EmbedBuilder()
       .setTitle("Farwater Servers")
