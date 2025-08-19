69f123004bade4bde3faaa83e2d43259a6e08aa1
Janko <161398674+Janko1902@users.noreply.github.com>
2025-08-19 06:32:26 +0200

    add personalized desc for staff

--- src/ghost-ping.js
+++ src/ghost-ping.js
@@ -1,60 +1,115 @@
+require("dotenv").config();
 const { EmbedBuilder } = require("discord.js");
 const client = require("./index.js");
+
+
+const chaosDesc = [
+    "{author} summoned {user}",
+    "{user} was summoned by {author}",
+    "{user} has been pinged by the elusive {author}",
+];
+
+const danDesc = [
+
+];
+
+const jankoDesc = [
+    "{author} said kaboom to {user}",
+    "{author} demonstrated boop on {user}",
+    "{author} said soup to {user}",
+    "{author} said <:soup:1406663240280445019> to {user}",
+    "{author} un-souped {user}",
+    "{user} might ban {author}",
+    "{user} will ban {author}",
+    "{user} banned {author}",
+];
+
+const greenDesc = [
+];
+
+
+const spudDesc = [
+    "{author} deep fried {user} for to long",
+];
+
+const owlDesc = [
+    "{author} hooted at {user}",
+    "{author} gave a hoot to {user}",
+];
+
+const latteDesc = [
+
+];
+
+const descriptions = [
+    "{author} pinged {user}",
+    "{user} was pinged by {author}",
+    "{author} gave {user} a ghost ping!",
+    "{user} got ghost pinged by {author}",
+    "{author} sent a ping to {user}, but it vanished",
+    "{user} was poked by Lord {author}",
+    "{author} woke {user} up",
+    "{user} was kicked by the wandering {author}",
+    "{author} demonstrated boop on {user}",
+    "{author} said soup to {user}",
+    "{author} said <:soup:1406663240280445019> to {user}",
+    "{author} un-souped {user}",
+    "{author} hooted at {user}",
+    "{author} gave a hoot to {user}",
+    "{author} whispered a ping to {user}",
+    "{author}'s message haunted {user}",
+    "{user} received a mysterious signal from {author}",
+    "{author} summoned {user}",
+    "{user} was summoned by {author}",
+    "{author} booped {user} and ran!",
+    "{user} has been pinged by the elusive {author}",
+];
+
+const userDescMap = {
+    [process.env.CHAOS_ID]: chaosDesc,
+    [process.env.DAN_ID]: danDesc,
+    [process.env.JANKO_ID]: jankoDesc,
+    [process.env.GREEN_ID]: greenDesc,
+    [process.env.SPUD_ID]: spudDesc,
+    [process.env.OWL_ID]: owlDesc,
+    [process.env.LATTE_ID]: latteDesc
+};
 
 const ghostPing = new Map();
 
 client.on("messageCreate", (msg) => {
-const userMentions = msg.mentions.users.filter(user => user.id !== msg.author.id);
-
-if (userMentions.size > 0) {
-    ghostPing.set(msg.id, {
-    author: msg.author.id,
-    mentions: [...userMentions.keys()],
-    channel: msg.channel.id,
-    });
-    setTimeout(() => ghostPing.delete(msg.id), 600000);
-}
+    const userMentions = msg.mentions.users.filter(user => user.id !== msg.author.id);
+    if (userMentions.size > 0) {
+        ghostPing.set(msg.id, {
+            author: msg.author.id,
+            mentions: [...userMentions.keys()],
+            channel: msg.channel.id,
+        });
+        setTimeout(() => ghostPing.delete(msg.id), 600000);
+    }
 });
 
 client.on("messageDelete", async (msg) => {
     const cached = ghostPing.get(msg.id);
     if (!cached) return;
 
-    const user = cached.mentions.map(uid => `<@${uid}>`).join(", ");
+    const pingedUser = cached.mentions;
     const channel = await client.channels.fetch(cached.channel);
 
-    const descriptions = [
-        "{author} pinged {user}",
-        "{user} was pinged by {author}",
-        "{author} gave {user} a ghost ping!",
-        "{user} got ghost pinged by {author}",
-        "{author} sent a ping to {user}, but it vanished",
-        "{user} wad poked by Lord {author}",
-        "{author} woke {user} up",
-        "{user} was kicked by the wandering {author}",
-        "{author} demonstrated boop on {user}",
-        "{author} said soup to {user}",
-        "{author} said <:soup:1406663240280445019> to {user}",
-        "{author} un-souped {user}",
-        "{author} hooted at {user}",
-        "{author} gave a hoot to {user}",
-        "{author} whispered a ping to {user}",
-        "{author}'s message haunted {user}",
-        "{user} received a mysterious signal from {author}",
-        "{author} whispered something to {user}",
-        "{author} booped {user} and ran!",
-        "{author} summoned {user}",
-    ]
+    const descArr =
+        userDescMap[pingedUser[0]] && userDescMap[pingedUser].length > 0
+        ? userDescMap[pingedUser]
+        : descriptions;
 
-    let chosenEmbed = descriptions[Math.floor(Math.random() * descriptions.length)]
+    const userMentions = pingedUser.map(uid => `<@${uid}>`).join(", ");
 
-    chosenEmbed = chosenEmbed.replaceAll("{author}", `<@${cached.author}>`);
-    chosenEmbed = chosenEmbed.replaceAll("{user}", user);
+    let chosenDesc = descArr[Math.floor(Math.random() * descArr.length)];
+    chosenDesc = chosenDesc.replaceAll("{author}", `<@${cached.author}>`).replaceAll("{user}", userMentions);
 
     const embed = new EmbedBuilder()
         .setColor("#ff4949")
         .setTitle("<:warning:1406652017165996062> Ghost Ping Detected!")
-        .setDescription(chosenEmbed)
+        .setDescription(chosenDesc)
         .setTimestamp();
 
     await channel.send({ embeds: [embed] });
